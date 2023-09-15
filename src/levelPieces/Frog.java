142ad97b9330f5ec73867c4d92269b1c587cfa28
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-15 14:27:25 -0600

    Created Frog class with constructor, added prototype for interaction

---
+++ src/levelPieces/Frog.java
@@ -0,0 +1,22 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class Frog extends GamePiece {
+
+	public Frog(char symbol, String label, int location) {
+		super(symbol, label, location);
+		
+		
+		
+		
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
