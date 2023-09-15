45ac1f4dcfe9612aff1f1ba7e52b2891bc7c19f4
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-15 15:27:49 -0600

    Added comments on all funciton prototypes in LevelSetup.java

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -19,22 +19,35 @@ public class LevelSetup {
 	public void createLevel(int levelNum) {
 		
 	}
-	
+	/**
+	 * This function provides the working gameBoard array that will be used to run the game. 
+	 * @return The final array that will be used to store the gamestate
+	 */
 	public Drawable [] getBoard() {
 		
 		
 		return null;
 	}
-	
+	/**
+	 * This function is called to build the array of pieces that have the capability of moving. 
+	 * @return The array of moving pieces
+	 */
 	public ArrayList<Moveable> getMovingPieces() {
 		
 		return null;
 	}
-	
+	/**
+	 * This funciton is called to build the array of pieces that can interact with the player. 
+	 * @return The array of interacting pieces. 
+	 */
 	public ArrayList<GamePiece> getInteractingPieces() {
 		
 		return null;
 	}
+	/**
+	 * This function is called to provide the players starting location. 
+	 * @return
+	 */
 	public int getPlayerStartLoc() {
 		
 		return 0;
