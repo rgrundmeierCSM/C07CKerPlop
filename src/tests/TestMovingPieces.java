d1a964d5ae886420cc49d3dc23203f715ed7c4f7
rgrundmeierCSM <rgrundmeier@mines.edu>
2023-09-19 15:09:42 -0600

    Implemented Testing for Assassin Movement

--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -1,13 +1,41 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import gameEngine.Drawable;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import gameEngine.GameEngine;
+import gameEngine.Moveable;
+import levelPieces.LevelSetup;
+import levelPieces.Assassin;
 
 public class TestMovingPieces {
 	@Test
+	
 	public void testAssassin() {
-		assertEquals(1,1);
+		Moveable [] board = new Moveable[GameEngine.BOARD_SIZE];
+		
+		// Insert the assassin at position 6
+		board[6] = new Assassin(6);
+		
+		//Assuming the player is at position 18, test that the assassin takes 1 step movements toward the player. 
+		
+		for (int i = 6; i < 14; i++) {
+			board[i].move(board, 18);
+			assertTrue(board[i+1] != null);
+			
+		}
+		
+		//Assuming the player is at position 3, test that the Assassin takes 1 step toward the player (left)
+		
+		for (int i = 14; i > 5; i--) {
+			board[i].move(board, 3);
+			assertTrue(board[i-1] != null);
+		}
 	}
 	@Test
 	public void testGoldenSnitch() {
