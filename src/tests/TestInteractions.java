a6d4fee09e44ac95f1038b3ddc76233c821ca04d
ClarkTHoward <clark.t.howard@gmail.com>
2023-09-19 14:22:27 -0600

    Basic test setup

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -3,34 +3,47 @@ package tests;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
 import levelPieces.Platform;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 import gameEngine.Drawable;
 
 public class TestInteractions {
+	@Test
 	public void testAssassin() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testGoldenSnitch() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testGuard() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testMysteriousClock() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testPlatform() {
 		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 		Platform platform = new Platform(10);
 		gameBoard[10] = platform;
-		assertEquals(InteractionResult.NONE,platform.interact(gameBoard, 10);
+		assertEquals(InteractionResult.NONE,platform.interact(gameBoard, 10));
 	}
+	@Test
 	public void testPoisonFrog() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testPrize() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testTreasure() {
-
+		assertEquals(1,1);
 	}
 }
--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -1,16 +1,24 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TestMovingPieces {
+	@Test
 	public void testAssassin() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testGoldenSnitch() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testGuard() {
-
+		assertEquals(1,1);
 	}
+	@Test
 	public void testPoisonFrog() {
-
+		assertEquals(1,1);
 	}
 }
