e9c68279aa7deaab92b5610e30fd048f30ff5c4c
ClarkTHoward <clark.t.howard@gmail.com>
2023-09-19 14:18:52 -0600

    worked on getting first test to work

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -1,5 +1,10 @@
 package tests;
 
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.Platform;
+import gameEngine.Drawable;
+
 public class TestInteractions {
 	public void testAssassin() {
 
@@ -14,7 +19,10 @@ public class TestInteractions {
 
 	}
 	public void testPlatform() {
-
+		Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Platform platform = new Platform(10);
+		gameBoard[10] = platform;
+		assertEquals(InteractionResult.NONE,platform.interact(gameBoard, 10);
 	}
 	public void testPoisonFrog() {
 
